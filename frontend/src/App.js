11611a08f79833eb6a4b51b6d8f3ecb343f58d38
Florian <florian.7799@hotmail.fr>
2025-05-28 10:58:40 +0200

    Ajout du routing et pages correspondantes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Accueil from "./pages/Accueil";
+import Chantier from "./pages/Chantier";
+import Admin from "./pages/Admin";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Accueil />} />
+        <Route path="/chantier/:id" element={<Chantier />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </Router>
   );
 }
 
