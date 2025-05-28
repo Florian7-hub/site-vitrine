6ed6229777132fe3262515bf9e40ab105ebd01c0
Florian <florian.7799@hotmail.fr>
2025-05-28 16:30:45 +0200

    Correction et validation des tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders Nos Chantiers title", () => {
+  render(<App />); // Supprime BrowserRouter ici
+
+  const headerElement = screen.getByText(/Nos Chantiers/i);
+  expect(headerElement).toBeInTheDocument();
 });
